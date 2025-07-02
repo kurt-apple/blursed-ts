6e5ca9a3556b8cd51d3e7ef3a38c46ca86b07c91
Kurt Apple <noreply@kurtapple.com>
2025-07-02 12:09:10 -0700

    just... beautiful

---
+++ array.ts
@@ -0,0 +1,18 @@
+declare global {
+  interface Array<T> {
+    sum: (K?: keyof T | ((item: T) => any)) => number
+  }
+}
+
+Array.prototype.sum = function<T>(K?: keyof T | ((item: T) => any)) {
+  if (K) {
+    if (typeof K === 'function') {
+      return this.map(K).sum()
+    }
+    this.map(x => x[K]).sum()
+  }
+  if(this.some(x => {
+    typeof x === 'undefined' || typeof x === 'symbol'
+  })) return 0
+  return this.reduce((acc, item) => acc + item, 0);
+}
